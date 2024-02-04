8c86e0f5c2f6fdca0b659badc9af1d76da93238e
Scarlett338 <wordsfromheart338@gmail.com>
2024-02-04 20:46:31 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    numOfppl: 0,
+  });
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+    setSubmitted(true);
+    setForm({
+      name: "",
+      email: "",
+      numOfppl: 0,
+    });
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!submitted && (
+        <form onSubmit={handleSubmit}>
+          <h1>Booking a Table</h1>
+          <div className="input">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={handleChange}
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div className="input">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleChange}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="numOfppl">Number of guests:</label>
+            <select name="numOfppl" id="numOfppl" required>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <div className="input">
+            <label htmlFor="time">Time:</label>
+            <select name="time" id="time" required>
+              <option>17:00</option>
+              <option>18:00</option>
+              <option>19:00</option>
+              <option>20:00</option>
+              <option>21:00</option>
+              <option>22:00</option>
+            </select>
+          </div>
+
+          <button type="submit">submit</button>
+        </form>
+      )}
+
+      {submitted && <PrintForm />}
     </div>
   );
 }
 
 export default App;
+
+function PrintForm() {
+  return (
+    <>
+      <h2>Your Booking is confirmed!</h2>
+    </>
+  );
+}
